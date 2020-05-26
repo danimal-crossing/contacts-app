294108d7b67ef62dff29d379803425c80d7726ac
dani <dani.zaghian@gmail.com>
2020-05-26 12:34:10 -0700

    adds single and all contacts web requests

--- app/controllers/api/contacts_controller.rb
+++ app/controllers/api/contacts_controller.rb
@@ -1,5 +1,13 @@
 class Api::ContactsController < ApplicationController
 
+  def one_contact_action
+    @contact = Contact.first #single contact hash
+    render 'one_contact.json.jb'
+  end
 
+  def all_contacts_action
+    @contacts = Contact.all #array of contact hashes
+    render 'all_contacts.json.jb'
+  end
 
 end
